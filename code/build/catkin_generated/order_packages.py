69c59925692fe4c7b7537dbb2cd15eaa702fae04
KocurMaros <kocur.maros@gmail.com>
2022-10-15 19:11:54 +0200

    nanovo

--- code/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/laptop/BakalarskaPraca/code/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/laptop/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/laptop/catkin_ws/devel;/opt/ros/noetic' != '' else []
