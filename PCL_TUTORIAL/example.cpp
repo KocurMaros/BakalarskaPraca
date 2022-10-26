10b033c7e48f743c239ffadfb9a3a37fe7e0e447
KocurMaros <kocur.maros@gmail.com>
2022-10-26 10:12:50 +0200

    XYZ / RGB / Normals

--- PCL_TUTORIAL/example.cpp
+++ PCL_TUTORIAL/example.cpp
@@ -413,7 +413,15 @@ main (int argc, char** argv)
     printUsage (argv[0]);
     return 0;
   }
-  bool simple(false), rgb(false), custom_c(false), normals(false),
+    pcl::PointCloud<pcl::PointXYZ>::Ptr basic_cloud_ptr (new pcl::PointCloud<pcl::PointXYZ>);
+
+    if (pcl::io::loadPCDFile<pcl::PointXYZ> ("test_pcd.pcd", *basic_cloud_ptr) == -1) //* load the file
+  {
+    PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
+    return (-1);
+  }
+
+  bool simple(false), rgb(false), custom_c(false), normals( false),
     shapes(false), viewports(false), interaction_customization(false);
   if (pcl::console::find_argument (argc, argv, "-s") >= 0)
   {
@@ -459,42 +467,41 @@ main (int argc, char** argv)
   // ------------------------------------
   // -----Create example point cloud-----
   // ------------------------------------
-  pcl::PointCloud<pcl::PointXYZ>::Ptr basic_cloud_ptr (new pcl::PointCloud<pcl::PointXYZ>);
+  // pcl::PointCloud<pcl::PointXYZ>::Ptr basic_cloud_ptr (new pcl::PointCloud<pcl::PointXYZ>);
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr point_cloud_ptr (new pcl::PointCloud<pcl::PointXYZRGB>);
   std::cout << "Generating example point clouds.\n\n";
   // We're going to make an ellipse extruded along the z-axis. The colour for
   // the XYZRGB cloud will gradually go from red to green to blue.
-  std::uint8_t r(255), g(15), b(15);
-  for (float z(-1.0); z <= 1.0; z += 0.05)
+  std::uint8_t r(255), g(0), b(0);
+  std::cout << "size " << basic_cloud_ptr->points.size() << endl;
+  for (int i=0; i < basic_cloud_ptr->points.size(); i++ )
   {
-    for (float angle(0.0); angle <= 360.0; angle += 5.0)
-    {
-      pcl::PointXYZ basic_point;
-      basic_point.x = 0.5 * std::cos (pcl::deg2rad(angle));
-      basic_point.y = sinf (pcl::deg2rad(angle));
-      basic_point.z = z;
-      basic_cloud_ptr->points.push_back(basic_point);
-
-      pcl::PointXYZRGB point;
-      point.x = basic_point.x;
-      point.y = basic_point.y;
-      point.z = basic_point.z;
-      std::uint32_t rgb = (static_cast<std::uint32_t>(r) << 16 |
-              static_cast<std::uint32_t>(g) << 8 | static_cast<std::uint32_t>(b));
-      point.rgb = *reinterpret_cast<float*>(&rgb);
-      point_cloud_ptr->points.push_back (point);
-    }
-    if (z < 0.0)
-    {
-      r -= 12;
-      g += 12;
-    }
-    else
-    {
-      g -= 12;
-      b += 12;
-    }
+    
+    pcl::PointXYZRGB point;
+    point.x = basic_cloud_ptr->points[i].x;
+    point.y = basic_cloud_ptr->points[i].y;
+    point.z = basic_cloud_ptr->points[i].z;
+    std::uint32_t rgb = (static_cast<std::uint32_t>(r) << 16 |
+            static_cast<std::uint32_t>(g) << 8 | static_cast<std::uint32_t>(b));
+    point.rgb = *reinterpret_cast<float*>(&rgb);
+    point_cloud_ptr->points.push_back (point);
+    if((i%1927)==0){
+      r-=1;
+      g+=1;
+      b+=1;
+    }    
+    // if (basic_cloud_ptr->points[i].z < )
+    // {
+    //   r -= 12;
+    //   g += 12;
+    // }
+    // else
+    // {
+    //   g -= 12;
+    //   b += 12;
+    // }
   }
+  
   basic_cloud_ptr->width = basic_cloud_ptr->size ();
   basic_cloud_ptr->height = 1;
   point_cloud_ptr->width = point_cloud_ptr->size ();
@@ -557,4 +564,5 @@ main (int argc, char** argv)
   //   viewer->spinOnce (100);
   //   std::this_thread::sleep_for(100ms);
   // }
+  
 }
