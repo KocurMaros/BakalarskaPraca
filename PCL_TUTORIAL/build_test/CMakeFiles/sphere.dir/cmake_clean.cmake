4b51214301316ac1102b11ed456b7e0afb808d98
KocurMaros <kocur.maros@gmail.com>
2023-03-12 12:29:46 +0100

    rozbehane PCL

---
+++ PCL_TUTORIAL/build_test/CMakeFiles/sphere.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sphere.dir/sphere.cpp.o"
+  "CMakeFiles/sphere.dir/sphere.cpp.o.d"
+  "sphere"
+  "sphere.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sphere.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
